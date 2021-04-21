7e2eab9fe220233044ff754e4470bde2d43fd558
Jimjule <roadknightj@gmail.com>
2021-04-21 16:37:08 +0100

    Cleans up the server test

--- src/test/java/EchoServerTest.java
+++ src/test/java/EchoServerTest.java
@@ -18,8 +18,6 @@ class EchoServerTest {
     private ArrayList<String> out = new ArrayList<>();
     private ArrayList<String> in = new ArrayList<>();
 
-
-
     @BeforeEach
     public void setup() throws IOException {
         echoClient = new EchoClientSpy();
@@ -29,17 +27,17 @@ class EchoServerTest {
         in.add(echoClient.sendMessage("Roger"));
         in.add(echoClient.sendMessage("and"));
         in.add(echoClient.sendMessage("."));
-        in.add(echoClient.sendMessage("If you can see this, it's all gone wrong"));
+        in.add(echoClient.sendMessage("If this gets returned, it's all gone wrong"));
     }
 
     @Test
-    public void serverEchoesInputUntilFullStop() throws IOException {
+    public void serverEchoesInputUntilFullStop() {
         echoServer = new EchoServerSpy(serverSocket, in, out);
         echoServer.start();
+        assertEquals(3, out.size());
         assertEquals("Roger", out.get(0));
         assertEquals("and", out.get(1));
         assertEquals("Stopping.", out.get(2));
-        assertEquals(3, out.size());
         assertEquals(3, ((EchoServerSpy) echoServer).respondCalledTimes);
     }
 
@@ -50,8 +48,7 @@ class EchoServerTest {
         echoClient.stop();
         try {
             echoServer.getServerSocket().close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ignored) {
         }
     }
 }
